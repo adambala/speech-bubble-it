aeecb567a97888fb0ee3be4a26cb3d0e6c7a5ac8
Adam Isakov <99123480+adambala@users.noreply.github.com>
2024-01-13 00:08:27 +0300

    refactor: speech_bubble_it - made code PEP 8 compliant

--- speech_bubble_it.py
+++ speech_bubble_it.py
@@ -11,8 +11,8 @@ SPEECH_BUBBLE_FILENAME = "speech_bubble.png"
 
 # formats known to be supported
 SUPPORTED_FORMATS = (
-    ".png", 
-    ".jpg", 
+    ".png",
+    ".jpg",
     ".jpeg",
     ".gif",
     ".bmp",
@@ -27,7 +27,9 @@ def check_file_format(file_path: Path, supported_formats: tuple) -> None:
     """
 
     if file_path.suffix not in supported_formats:
-        raise ValueError(f"Unsupported file extension {file_path.suffix}. Only {supported_formats} are supported.")
+        raise ValueError(
+            f"Unsupported file extension {file_path.suffix}. Only {supported_formats} are supported."
+        )
 
 
 def open_image(image_path: Path) -> Image.Image:
@@ -37,7 +39,7 @@ def open_image(image_path: Path) -> Image.Image:
 
     with Image.open(image_path) as img:
         img.load()
-        
+
     return img
 
 
@@ -50,7 +52,7 @@ def transform_image(img: Image.Image, mirror: bool, orientation: int) -> Image.I
         img = img.transpose(Image.FLIP_LEFT_RIGHT)
 
     match orientation:
-        case 1: pass # default orientation
+        case 1: pass  # default orientation
         case 2: img = img.transpose(Image.ROTATE_90)
         case 3: img = img.transpose(Image.ROTATE_180)
         case 4: img = img.transpose(Image.ROTATE_270)
@@ -62,19 +64,25 @@ def process(image_path: Path, output_path: Path, mirror: bool, orientation: int)
     """
     Adds a speech bubble on image and saves it.
     """
-    
+
     if not image_path.is_file():
         raise FileNotFoundError(f"Image file {image_path} not found")
 
     check_file_format(image_path, SUPPORTED_FORMATS)
     check_file_format(output_path, SUPPORTED_FORMATS)
-    
-    original_image = open_image(image_path).convert("RGBA") # RGBA to ensure transperency in the result
-    speech_bubble_image = open_image(Path(os.path.join(ASSETS_DIR, SPEECH_BUBBLE_FILENAME)))
 
-    ImageOps.exif_transpose(original_image, in_place=True) # to prevent unexpected rotation of images with EXIF orientation tag != 1
+    original_image = open_image(image_path).convert(
+        "RGBA"  # RGBA to ensure transperency in the result
+    )
+    speech_bubble_image = open_image(
+        Path(os.path.join(ASSETS_DIR, SPEECH_BUBBLE_FILENAME)))
+
+    # to prevent unexpected rotation of images with EXIF orientation tag != 1
+    ImageOps.exif_transpose(original_image, in_place=True)
     speech_bubble_image = speech_bubble_image.resize(original_image.size)
-    speech_bubble_image = transform_image(speech_bubble_image, mirror, orientation)
+    speech_bubble_image = transform_image(
+        speech_bubble_image, mirror, orientation
+    )
 
     result = ImageChops.subtract_modulo(original_image, speech_bubble_image)
 
@@ -102,12 +110,16 @@ def main() -> None:
         format="%(levelname)s: %(message)s",
         level=logging.INFO,
     )
-    
+
     parser = ArgumentParser()
-    parser.add_argument("image_path", type=Path, metavar="image_file", help="path of an image you want to put speech bubble on")
-    parser.add_argument("output_path", type=Path, metavar="output_file", help="path to the file where the result will be written to")
-    parser.add_argument("-m", "--mirror", action=BooleanOptionalAction, help="flips speech bubble horizontally")
-    parser.add_argument("-o", "--orientation", type=int, default=1, choices=(1, 2, 3, 4), help="changes speech bubble orientation (1: on the top; 2: on the left; 3: on the bottom; 4: on the right)")
+    parser.add_argument("image_path", type=Path, metavar="image_file",
+                        help="path of an image you want to put speech bubble on")
+    parser.add_argument("output_path", type=Path, metavar="output_file",
+                        help="path to the file where the result will be written to")
+    parser.add_argument("-m", "--mirror", action=BooleanOptionalAction,
+                        help="flips speech bubble horizontally")
+    parser.add_argument("-o", "--orientation", type=int, default=1, choices=(1, 2, 3, 4),
+                        help="changes speech bubble orientation (1: on the top; 2: on the left; 3: on the bottom; 4: on the right)")
     args = parser.parse_args()
 
     process(**vars(args))
